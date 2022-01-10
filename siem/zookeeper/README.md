4a6bf11751b2229b9c53ee9802234f6e29fc15a2
Jerome Xue <kongpo0412@gmail.com>
2022-01-10 11:29:50 +0800

    first commit

---
+++ siem/zookeeper/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - siem.zookeeper
+
+Documentation for the collection.
---
+++ tests/test_siem_playbook.py
@@ -0,0 +1,5 @@
+from siem_playbook import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
